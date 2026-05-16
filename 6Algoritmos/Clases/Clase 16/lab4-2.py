9605fd7df1a1bf8eab71576a9ec9348bd7fb6084
malgohip <133774889+malgohip@users.noreply.github.com>
2026-05-16 17:42:04 +0000

    terminado

--- 6Algoritmos/Clases/Clase 16/lab4-2.py
+++ 6Algoritmos/Clases/Clase 16/lab4-2.py
@@ -1,17 +1,40 @@
 def mergeSort(A,p,r):
     if (p>=r):
-        return
+        print(' '.join(map(str,A[p:r+1])))
+        return A
+    print(' '.join(map(str,A[p:r+1])))
     q=int((p+r)/2)
-    mergeSort(A,p,q)
     mergeSort(A,q+1,r)
+    mergeSort(A,p,q)
     merge(A,p,q,r)
+    print(' '.join(map(str,A[p:r+1])))
+    return A
 
 def merge(A,p,q,r):
     nl, nr=q-p+1, r-q
-    L, R=[], []
-    for i in range(0,nl-1):
-        L[i]=A[p+i]
-    for j in range(0,nr-1):
-        R[j]=A[q+j+1]
+    L, R=[],[]
+    for i in range(nl):
+        L.append(A[p + i])
+    for j in range(nr):
+        R.append(A[q + j + 1])
     i, j, k = 0, 0, p
+    while(i<nl and j<nr):
+        if L[i]>=R[j]:
+            A[k]=L[i]
+            i+=1
+        else:
+            A[k]=R[j]
+            j+=1
+        k+=1
+    while(i<nl):
+        A[k]=L[i]
+        i+=1
+        k+=1
+    while(j<nr):
+        A[k]=R[j]
+        j+=1
+        k+=1
 
+A=list(map(int,input().strip().split()))
+n=len(A)
+mergeSort(A,0,n-1)
